4b86eab4ecf7967628743a36d10a16a517dfda4e
Jenyyk <jendotron2@gmail.com>
2025-03-16 15:09:42 +0100

    refactor: refactor main.rs

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,14 @@ use tokio;
 
 #[tokio::main]
 async fn main() {
+    let new_mails = vec!["jan.krivsky@maturak26ab.cz"];
+
     let mut client = MailClient::new().await.unwrap();
-    client.send_formatted_mail(
-        vec!["jan.krivsky@maturak26ab.cz"],
-        3_u8,
-        186_u16
-    ).await;
+    for address in new_mails {
+        let _ = client.send_formatted_mail(
+            vec![address],
+            3_u8,
+            186_u16
+        ).await;
+    }
 }
